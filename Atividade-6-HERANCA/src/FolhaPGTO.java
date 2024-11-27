38986b482e8e15fc764a7277d8381fd06a98b497
Thata1994 <136244602+Thata1994@users.noreply.github.com>
2024-11-27 10:00:26 -0300

    Update FolhaPGTO.java

--- Atividade-6-HERANCA/src/FolhaPGTO.java
+++ Atividade-6-HERANCA/src/FolhaPGTO.java
@@ -60,15 +60,5 @@ public class FolhaPGTO {
         System.out.println("INSS: " + hora2.calcularINSS());
         System.out.println("IR: " + hora2.calcularIR());
 
-        
-        // Mensalista m1 = new Mensalista("José", "Rua abc n345", "Junior");
-        // Horista h1 = new Horista("Maria", "Rua 23 de maio", 10);
-        // m1.calcularSalario();
-        // System.out.println("INSS: "+m1.calcularINSS());
-        // System.out.println("IR: "+m1.calcularIR());
-        
-        // h1.calcularSalario();
-        // System.out.println("INSS: "+h1.calcularINSS());
-        // System.out.println("IR: "+h1.calcularIR());
     }
 }
